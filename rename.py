eaaefe459a80b78d32e1902f441b3cbd981de541
Richmond Rice <richmond@ricey.org>
2017-07-13 21:40:31 +0100

    Update rename.py

--- rename.py
+++ rename.py
@@ -5,4 +5,10 @@ path = os.getcwd() # get current working directory
 filenames = os.listdir(path)
 
 for filename in filenames:
-    os.rename(filename, filename.replace("1080p", "").lower()
+    os.rename(filename, filename.replace("1080p", "").lower())
+
+filenames = os.listdir(path)
+	
+for filename in filenames:
+    os.rename(filename, filename.replace("720p", "").lower())
+	
