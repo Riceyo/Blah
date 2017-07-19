082ab227bdc95d227aca5ab2c3ea8e3695775d15
Richmond Rice <richmond@ricey.org>
2017-07-19 22:58:25 +0100

    Create tesco_groceries_api_loop.py

---
+++ tesco_groceries_api_loop.py
@@ -0,0 +1,23 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64, time
+
+headers = {'Ocp-Apim-Subscription-Key': 'c83fdbe1c5e84c7aabe2ee0a6b4f181f'}
+
+try:
+	conn = http.client.HTTPSConnection('dev.tescolabs.com')
+	for loop in range(264781000, 264789999, 1):
+		time.sleep(1)
+		params = urllib.parse.urlencode({'tpnc': loop})
+		conn.request("GET", "/product/?%s" % params, "{body}", headers)
+		response = conn.getresponse()
+		data = str(response.read())
+		formatdata = " ".join(data.split()).replace(r"\r\n", "").strip()
+		#print(formatdata)
+		if (formatdata != "b\'{ \"products\": []}\'"):
+			if ("isFood\": false, \"isDrink\": false" not in formatdata):
+				fileobj = open("blah.txt", "a")
+				fileobj.write(formatdata)
+				fileobj.write("\n")
+				fileobj.close
+	conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
