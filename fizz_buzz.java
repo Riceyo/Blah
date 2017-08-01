c89e5dc6f24fdef599dd34c02ae0d0cefcef9881
Richmond Rice <richmond@ricey.org>
2017-08-01 20:13:46 +0100

    Update fizz_buzz.java

--- fizz_buzz.java
+++ fizz_buzz.java
@@ -28,7 +28,8 @@ public class FizzBuzz // seeing how I approach the fizz buzz test
 		int loop;
 		for (loop = 1; loop < 101; loop++)
 		{
-			// System.out.println(loop + " " + Fizz(loop) + Buzz(loop)); // didn't realise number shouldn't be shown if fizz or buzz
+			// System.out.println(loop + " " + Fizz(loop) + Buzz(loop));
+			// didn't realise number shouldn't be shown if fizz or buzz
 			String print = "";
 			print = Fizz(loop);
 			print = print + Buzz(loop);
