76c06d55dfe8dffbf56847cb4d430a849b91ac4a
Richmond Rice <richmond@ricey.org>
2017-08-06 16:52:27 +0100

    Update ExampleInterfaceCarMain.java

--- ExampleInterfaceCarMain.java
+++ ExampleInterfaceCarMain.java
@@ -1,4 +1,4 @@
-nbvnbvn
+
 public class ExampleInterfaceCarCall {
 	public static void main(String args[]) {
 		ExampleInterfaceCar VWGolfGTI = new ExampleInterfaceCarVWGolfGTI(); // create object from the class constructor and assign it reference variable of the interface
