aeaca3148f7c748f28f26886e30fbc650852c580
Richmond Rice <richmond@ricey.org>
2017-08-17 20:23:49 +0100

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,13 +1,18 @@
 import os
 
-path = os.getcwd() # get current working directory
+dlpath = "N:\\downloads\\test\\"
+replacewithspace = {".", ",", "-", "_", "(", ")", "[", "]", "{", "}"}
+replacewithnothing = {"1080p", "720p", "480p", "brrip", "hdrip", "webrip", "dvdrip", "dvdscr"}
 
-filenames = os.listdir(path)
+filenames = os.listdir(dlpath)
 
 for filename in filenames:
-    os.rename(filename, filename.replace("1080p", "").lower())
-
-filenames = os.listdir(path)
-	
-for filename in filenames:
-    os.rename(filename, filename.replace("720p", "").lower())
+    filenamebase = os.path.splitext(os.path.basename(filename))[0]
+    filenameext = os.path.splitext(os.path.basename(filename))[1]
+    for loop in replacewithspace:
+        filenamebase = filenamebase.replace(loop, " ")
+    for loop in replacewithnothing:
+        filenamebase = filenamebase.replace(loop, "")
+    filenamebase = " ".join(filenamebase.split()).strip().lower()
+    filenameext = filenameext.lower()    
+    os.rename(os.path.join(dlpath, filename), os.path.join(dlpath, filenamebase + filenameext))
