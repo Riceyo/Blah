6bb83eaaa6657173d6e75d75fc6b697635eac330
Richmond Rice <richmond@ricey.org>
2017-08-22 17:55:14 +0100

    Update ExampleInterfaceCar_main.java

--- ExampleInterfaceCar_main.java
+++ ExampleInterfaceCar_main.java
@@ -7,7 +7,7 @@ public class ExampleInterfaceCarCall {
 		VWGolfGTI.getMilesPerGallon();
 		VWGolfGTI.getPrice();
 		// VWGolfGTI.getEmissions(); // this method from the class cannot be used as it's not declared in the interface, we have no reference to it, security
-		ExampleInterfaceCar AudiS3 = new ExampleInterfaceCarAudiS3(); // create another object from a different class constructor but again assign it reference variable of the interface
+		ExampleInterfaceCar AudiS3 = new ExampleInterfaceCarAudiS3(); // create another object from a different class constructor but again assign it reference variable of the same interface
 		AudiS3.getNoughtToSixtyMilesPerHour();
 		AudiS3.getBrakeHorsepower();
 		AudiS3.getMilesPerGallon();
