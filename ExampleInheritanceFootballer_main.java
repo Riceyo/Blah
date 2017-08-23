cd18f796753e4ca180a5b2262ceb93a7e67311c3
Richmond Rice <richmond@ricey.org>
2017-08-23 21:13:54 +0100

    Update ExampleInheritanceFootballer_main.java

--- ExampleInheritanceFootballer_main.java
+++ ExampleInheritanceFootballer_main.java
@@ -2,19 +2,19 @@
 public class ExampleInheritanceFootballer_main {
 	public static void main(String[] args) {
 		ExampleInheritanceFootballerDefender cb = new ExampleInheritanceFootballerDefender(); // create object from a class that's inheriting data members and methods from another class
-		cb.Select(); // call overridden inherited method
+		cb.Select(); // call overridden inherited method (polymorphism)
 		cb.Position("Centre Back"); // call inherited method passing it a parameter
 		cb.Run(); // call inherited method
 		cb.Pass(); // call inherited method
 		cb.Shoot(); // call inherited method
 		ExampleInheritanceFootballerDefender lb = new ExampleInheritanceFootballerDefender(); // create another object but give it a different reference variable
-		lb.Select(); // call overridden inherited method
+		lb.Select(); // call overridden inherited method (polymorphism)
 		cb.Position("Left Back"); // call inherited method passing it a parameter
 		lb.Run(); // call inherited method
 		lb.Pass(); // call inherited method
 		lb.Shoot(); // call inherited method
 		ExampleInheritanceFootballerGoalkeeper gk = new ExampleInheritanceFootballerGoalkeeper(); // create another object from a different class that's inheriting from the same class as the other objects
-		gk.Select(); // call overridden inherited method
+		gk.Select(); // call overridden inherited method (polymorphism)
 		cb.Position("Goalkeeper"); // call inherited method passing it a parameter
 		gk.Run(); // call inherited method
 		gk.Pass(); // call inherited method
