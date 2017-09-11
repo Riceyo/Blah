5b4a3f21edbf9eb594c0dcfcc99c5dc460a06e64
Richmond Rice <richmond@ricey.org>
2017-09-11 19:17:21 +0100

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,16 +1,16 @@
 import os
 
-dlpath = "N:\\downloads\\test\\"
-replacewithspace = {".", ",", "-", "_", "(", ")", "[", "]", "{", "}"}
-replacewithnothing = {"1080p", "720p", "480p", "brrip", "hdrip", "webrip", "dvdrip", "dvdscr"}
+dlpath = "N:\\downloads\\"
+replacearray = (".", ",", "-", "_", "(", ")", "[", "]", "{", "}", \
+				"1080p", "720p", "480p", "brrip", "hdrip", "webrip", "dvdrip", "dvdscr", "hdtv", "h264", "x265", "xvid", "ac3", "800mb", \
+				"x264", "batv", "ettv", "webtube", "hevc", "megusta", "qpel", "mkvcage", "etrg", "bluray", "yify", "aac", \
+				"2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017")
 filenames = os.listdir(dlpath)
 for filename in filenames:
-    filenamebase = os.path.splitext(os.path.basename(filename))[0]
-    filenameext = os.path.splitext(os.path.basename(filename))[1]
-    for loop in replacewithspace:
+    filenamebase = os.path.splitext(os.path.basename(filename))[0].lower()
+    filenameext = os.path.splitext(os.path.basename(filename))[1].lower()
+    for loop in replacearray:
         filenamebase = filenamebase.replace(loop, " ")
-    for loop in replacewithnothing:
-        filenamebase = filenamebase.replace(loop, "")
-    filenamebase = " ".join(filenamebase.split()).strip().lower()
+    filenamebase = " ".join(filenamebase.split()).strip()
     filenameext = filenameext.lower()    
     os.rename(os.path.join(dlpath, filename), os.path.join(dlpath, filenamebase + filenameext))
