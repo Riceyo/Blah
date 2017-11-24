eb01048f72c2f251e3c433ae65c3b48f216254cb
Richmond Rice <richmond@ricey.org>
2017-11-24 19:23:15 +0000

    Update filename_imdb_lookup.py

--- filename_imdb_lookup.py
+++ filename_imdb_lookup.py
@@ -1,24 +1,27 @@
 import os
 import imdb
-
 def main():
-	dlpath = "N:\\media\\#\\"	
-	objimdb = imdb.IMDb()
-	filenames = os.listdir(dlpath)
+	moviepath = "N:\\media\\#\\"
+	spacer = " ; "
+	imdbobj = imdb.IMDb()
+	filenames = os.listdir(moviepath)
 	for filename in filenames:
-		filenamebase = os.path.splitext(os.path.basename(filename))[0].lower()
-		search = objimdb.search_movie(filenamebase)
+		filenamebase = os.path.splitext(os.path.basename(filename))[0]
+		search = imdbobj.search_movie(filenamebase)
 		res = search[0]
-		objimdb.update(res)		
-		outstr = res['long imdb title'] + "\t"
-		outstr += str(res['rating']) + "\t"
-		outstr += str(res['votes']) + "\t"
-		blah = res['runtime']
-		outstr += str(blah[0])	+ "\t"
-		#outstr = outstr + res['director']
-		#outstr = outstr + res['cast']
-		#outstr = outstr + res['genres']		
-		print(outstr)
-
+		imdbobj.update(res)
+		outputstr = res['long imdb title'] + spacer
+		outputstr += str(res['rating'])
+		outputstr += " (" + str(res['votes']) + ")" + spacer
+		tmp = res['genres']
+		outputstr += str(tmp[0]) + spacer
+		tmp = res['runtime']
+		outputstr += str(tmp[0]) + spacer
+		tmp = res['director']
+		outputstr += str(tmp[0]) + spacer
+		tmp = res['cast']
+		outputstr += str(tmp[0])
+		outputstr += ", " + str(tmp[1])
+		print(outputstr)
 if __name__ == '__main__':
     main()
